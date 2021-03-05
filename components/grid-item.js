00638629148f4b9690259e361c2d65b09ebc64d6
Amaraa <usuhuu1979@gmail.com>
2021-03-05 13:31:49 +0800

    slug, cover image, date нэмлээ

--- components/grid-item.js
+++ components/grid-item.js
@@ -1,6 +1,7 @@
 import { Card } from "react-bootstrap";
+import Link from "next/link";
 
-export default () => {
+export default ({ post }) => {
   return (
     <Card className={`fj-card`}>
       <div className="card-body-wrapper">
@@ -16,21 +17,21 @@ export default () => {
             <Card.Title className="font-weight-bold mb-1">
               Амар Өсөхбаяр
             </Card.Title>
-            <Card.Text className="card-date">2021 оны 3 сарын 2</Card.Text>
+            <Card.Text className="card-date">{post.date}</Card.Text>
           </div>
         </Card.Header>
-        <div className="view overlay">
-          <Card.Img
-            src="https://source.unsplash.com/user/erondu/250x250"
-            alt="Card image cap"
-          />
-        </div>
-        <Card.Body>
-          <Card.Title className="card-main-title">
-            Микросэрвис орчин үед
-          </Card.Title>
-          <Card.Text>Вэб технологи</Card.Text>
-        </Card.Body>
+
+        <Link href={`/${post.slug}`}>
+          <a>
+            <div className="view overlay">
+              <Card.Img src={post.image} alt="Card image cap" />
+            </div>
+            <Card.Body>
+              <Card.Title className="card-main-title">{post.title}</Card.Title>
+              <Card.Text>{post.subtitle}</Card.Text>
+            </Card.Body>
+          </a>
+        </Link>
       </div>
     </Card>
   );
